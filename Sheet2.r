14ce2567b4d51d300dca4299445bc486b5da9d75
Aayushi Ghosh <137275145+electricalpigeon@users.noreply.github.com>
2023-11-04 16:44:17 +0530

    Update Sheet2.r

--- Sheet2.r
+++ Sheet2.r
@@ -143,4 +143,164 @@ result <- insertion_sort(arr)
 print(result)
 
 
-#4...
+#4...Write a program to implement Towers of Hanoi algorithm to move ‘n’ disks from peg-1 to peg-2.
+
+#Write a function to implement towers of hanoi algorithm
+n<<-0 # defining n as global parameter.
+hanoi=function(p1,p2,p3,nd){
+	if(nd==1){
+		n<<-n+1
+		cat("Step-",n," : Move Disk-",nd," from peg-",p1," to peg-",p2,"\n")
+		return(n)
+	}else{ hanoi(p1,p3,p2,(nd-1))
+		n<<-n+1
+		cat("Step-",n," : Move Disk-",nd," from peg-",p1," to peg-",p2,"\n")
+		hanoi(p3,p2,p1,(nd-1))
+		}
+	}
+
+#5...Write a program to implement recursive call to calculate n-th member of a fibonacci series.
+#Fibonacci Function
+fib=function(a){
+n1=0
+n2=1
+if (a==1){ return(0)}
+if (a==2){return(1)}
+s=0
+for (i in 3:a){
+s=n1+n2
+n1=n2
+n2=s}
+return(s)
+}
+#display fibonacci function
+a<-as.integer(readline(" Enter your n: "))
+print(fib(a))
+
+
+#6...Write a program to store ‘n’ arbitrary integers in a list a[]. Calculate the frequency of all number and display all distinct numbers and its corresponding frequency.
+
+# Function to calculate and display frequency of distinct numbers
+calculate_frequency <- function(numbers) {
+  unique_numbers <- unique(numbers)  # Get distinct numbers
+  freq <- sapply(unique_numbers, function(x) sum(numbers == x))  # Calculate frequency
+  
+  cat("Distinct numbers and their frequencies:\n")
+  for (i in 1:length(unique_numbers)) {
+    cat("Number:", unique_numbers[i], " Frequency:", freq[i], "\n")
+  }
+}
+
+# Input the number of integers 'n'
+n <- as.integer(readline("Enter the number of integers (n): "))
+
+# Create an empty list to store integers
+a <- vector("integer", n)
+
+# Input 'n' integers
+for (i in 1:n) {
+  a[i] <- as.integer(readline(paste("Enter integer #", i, ": ")))
+}
+
+# Calculate and display the frequency of distinct numbers
+calculate_frequency(a)
+
+
+#7...Write a program to convert any +ve number(1-3000) and convert the number into corresponding Roman numerals.
+
+
+# Function to convert a positive number to Roman numerals
+number_to_roman <- function(num) {
+  if (num < 1 || num > 3000) {
+    return("Number out of range (1-3000)")
+  }
+
+  roman_numerals <- c("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
+  arabic_numbers <- c(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+
+  roman_numeral <- ""
+  i <- 1
+
+  while (num > 0) {
+    if (num >= arabic_numbers[i]) {
+      roman_numeral <- paste(roman_numeral, roman_numerals[i], sep = "")
+      num <- num - arabic_numbers[i]
+    } else {
+      i <- i + 1
+    }
+  }
+
+  return(roman_numeral)
+}
+
+# Input from the user
+number <- as.integer(readline("Enter a positive number (1-3000): "))
+
+# Convert the number to Roman numerals
+roman_numeral <- number_to_roman(number)
+
+# Display the result
+cat("Roman numeral:", roman_numeral, "\n")
+
+
+#8...Write a program to input any character and convert the character into bits and display character and its corresponding bits.
+# Input a character from the user
+char_input <- readline("Enter a character: ")
+
+# Convert the character to its binary representation
+binary_representation <- intToBits(charToRaw(char_input))
+
+# Display the character and its corresponding bits
+cat("Character:", char_input, "\n")
+cat("Binary Representation:", paste0(as.integer(binary_representation), collapse = ""), "\n")
+
+
+#9...Write a program in python to input ‘n’ (where n>=1 and n<=10) numbers. Apply Bubble Sort method to
+#sort those numbers and display sorted list. The program must also display how many comparisons required
+#to sort 10 elements. Extend the code to sort ‘n’ names in alphabetical order.
+
+
+# Function to perform Bubble Sort on a numeric vector
+bubbleSort <- function(arr) {
+  n <- length(arr)
+  comparisons <- 0
+  for (i in 1:(n - 1)) {
+    for (j in 1:(n - i)) {
+      if (arr[j] > arr[j + 1]) {
+        temp <- arr[j]
+        arr[j] <- arr[j + 1]
+        arr[j + 1] <- temp
+      }
+      comparisons <- comparisons + 1
+    }
+  }
+  return(list(sorted_list = arr, comparisons = comparisons))
+}
+
+# Input 'n' numbers from the user
+n <- as.integer(readline("Enter the value of n (between 1 and 10): "))
+if (n < 1 || n > 10) {
+  cat("n should be between 1 and 10.\n")
+} else {
+  numbers <- numeric(n)
+  names <- character(n)
+
+  for (i in 1:n) {
+    numbers[i] <- as.numeric(readline(paste("Enter number ", i, ": ")))
+  }
+
+  for (i in 1:n) {
+    names[i] <- readline(paste("Enter name ", i, ": "))
+  }
+
+  # Sort the numeric vector using Bubble Sort
+  result_numeric <- bubbleSort(numbers)
+  cat("Sorted numbers:", result_numeric$sorted_list, "\n")
+  cat("Number of comparisons required to sort numbers:", result_numeric$comparisons, "\n")
+
+  # Sort the names in alphabetical order
+  sorted_names <- sort(names)
+  cat("Sorted names:", sorted_names, "\n")
+}
+
+
